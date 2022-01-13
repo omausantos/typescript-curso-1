54c6bdfe73bc5ff877ce3c9be918b8944baec60d
Mauricio Santos <mauwebjc@gmail.com>
2022-01-12 22:38:18 -0300

    Conteúdos da Aula 03

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
-import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date(), 10, 1000);
-console.log('Data ->', negociacao.data);
-console.log('Quantidade ->', negociacao.quantidade);
-console.log('Valor ->', negociacao.valor);
-console.log('Volume ->', negociacao.volume);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
